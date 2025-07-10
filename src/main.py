60724592ea20c0ebd2ffd84cfa063170f9effc90
NE <NENightElves@qq.com>
2025-07-10 08:28:26 +0000

    update main.py

--- src/main.py
+++ src/main.py
@@ -9,7 +9,9 @@ app = flask.Flask(__name__)
 @app.route('/api/stock')
 def stock():
     stock_code = request.args.get('code')
-    ret_data = stock_util.calc_stock_metrics(stock_util.get_stock_data_by_days(stock_code)).to_dict('records')
+    df = stock_util.calc_stock_metrics(stock_util.get_stock_data_by_days(stock_code))
+    df['date'] = df['date'].apply(lambda x: x.strftime('%Y-%m-%d'))
+    ret_data = df.to_dict('records')
     return {"msg": "success", "data": ret_data}
 
 
