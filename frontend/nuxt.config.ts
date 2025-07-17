fd430075626996859bd4eca421876531c0a76db4
NE <NENightElves@qq.com>
2025-07-17 06:30:53 +0000

    init frontend

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@nuxt/ui'],
+  ssr: false,
+  css: ['~/assets/css/main.css'],
+  ui: {
+    fonts: false
+  },
+  runtimeConfig: {
+    public: {
+      baseUrl: process.env.BASE_URL || ''
+    }
+  }
+})
---
+++ frontend/pages/index.vue
@@ -0,0 +1,9 @@
+<script setup>
+const hello = ref('Hello World!')
+</script>
+
+<template>
+    <div>
+        {{ hello }}
+    </div>
+</template>
